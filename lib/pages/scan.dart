907fe2fdc77298b11daad62b8883fbba2670f34f
betbies <betniss11@gmail.com>
2024-09-17 13:07:57 +0300

    Değişiklik mesajı

--- lib/pages/scan.dart
+++ lib/pages/scan.dart
@@ -14,6 +14,43 @@ class _ScanPageState extends State<ScanPage> {
   bool _popupShown =
       false; // Pop-up'ın gösterilip gösterilmediğini takip eden değişken
 
+  void _showErrorPopup(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          backgroundColor: Colors.white
+              .withOpacity(0.5), // Arka plan rengini %50 şeffaf beyaz yapıyoruz
+          contentPadding:
+              EdgeInsets.zero, // İçeriğin etrafındaki boşlukları sıfırla
+          content: SizedBox(
+            width: 200, // Pop-up genişliği
+            height: 150, // Pop-up yüksekliği
+            child: Center(
+              child: Text(
+                'Yeni Pop-up', // Boş bir pop-up için basit bir metin
+                style: TextStyle(fontSize: 18, color: Colors.black),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
+          actions: [
+            TextButton(
+              style: TextButton.styleFrom(
+                foregroundColor:
+                    Colors.black, // Kapat butonunun metin rengi siyah
+              ),
+              child: const Text('Kapat'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   void _showPopup(BuildContext context) {
     showDialog(
       context: context,
@@ -43,10 +80,23 @@ class _ScanPageState extends State<ScanPage> {
                   child: Row(
                     children: [
                       Expanded(
-                        child: Container(
-                          padding: const EdgeInsets.all(10),
-                          color: Colors.red.withOpacity(
-                              0.5), // Arka plan rengini kırmızı ve %50 şeffaf yapıyoruz
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            padding: const EdgeInsets.all(10),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            backgroundColor: Colors.red.withOpacity(
+                                0.5), // Arka plan rengini kırmızı ve %50 şeffaf yapıyoruz
+                            elevation: 0, // Gölgeyi kaldır
+                          ).copyWith(
+                            foregroundColor: MaterialStateProperty.all(
+                                Colors.black), // Buton metin rengi siyah
+                          ),
+                          onPressed: () {
+                            Navigator.of(context).pop(); // İlk pop-up'ı kapat
+                            _showErrorPopup(context); // Yeni pop-up'ı aç
+                          },
                           child: const Center(
                             child: Text(
                               'Arıza Bildir',
@@ -60,10 +110,23 @@ class _ScanPageState extends State<ScanPage> {
                         color: Colors.black, // Siyah çizgi
                       ),
                       Expanded(
-                        child: Container(
-                          padding: const EdgeInsets.all(10),
-                          color: Colors.green.withOpacity(
-                              0.5), // Arka plan rengini yeşil ve %50 şeffaf yapıyoruz
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            padding: const EdgeInsets.all(10),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            backgroundColor: Colors.green.withOpacity(
+                                0.5), // Arka plan rengini yeşil ve %50 şeffaf yapıyoruz
+                            elevation: 0, // Gölgeyi kaldır
+                          ).copyWith(
+                            foregroundColor: MaterialStateProperty.all(
+                                Colors.black), // Buton metin rengi siyah
+                          ),
+                          onPressed: () {
+                            // Kontrol Edildi butonuna basıldığında yapılacak işlemler
+                            print('Kontrol Edildi butonuna basıldı');
+                          },
                           child: const Center(
                             child: Text(
                               'Kontrol Edildi',
@@ -80,6 +143,10 @@ class _ScanPageState extends State<ScanPage> {
           ),
           actions: [
             TextButton(
+              style: TextButton.styleFrom(
+                foregroundColor:
+                    Colors.black, // Kapat butonunun metin rengi siyah
+              ),
               child: const Text('Kapat'),
               onPressed: () {
                 Navigator.of(context).pop();
