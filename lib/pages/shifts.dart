43e1c2b1f78f0c030a6ba73150f325e23da2809d
betbies <betniss11@gmail.com>
2024-09-19 14:59:13 +0300

    Değişiklik mesajı

--- lib/pages/shifts.dart
+++ lib/pages/shifts.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class ShiftsPage extends StatelessWidget {
   const ShiftsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    int currentHour = DateTime.now().hour;
+    DateTime now = DateTime.now();
+    String formattedDate = "${now.day}-${now.month}-${now.year}";
+    String formattedTime = "${now.hour}:${now.minute}";
+
+    int currentHour = now.hour;
 
     // Shift'in otomatik olarak genişletilmesi için kontrol
     bool isShiftA = currentHour >= 8 && currentHour < 16;
@@ -129,9 +134,42 @@ class ShiftsPage extends StatelessWidget {
         centerTitle: true,
         backgroundColor: const Color(0xFFFCFBF5), // İstediğiniz renk
       ),
-      body: ListView(
-        padding: const EdgeInsets.all(16),
-        children: shifts,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: const EdgeInsets.all(16),
+            color: const Color(0xFFFCFBF5), // Arka plan rengi
+            child: Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    formattedDate,
+                    style: GoogleFonts.anta(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(width: 16),
+                  Text(
+                    formattedTime,
+                    style: GoogleFonts.anta(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              padding: const EdgeInsets.all(16),
+              children: shifts,
+            ),
+          ),
+        ],
       ),
     );
   }
